95f6f5246978f5ad8e6f49585297c225d83537d5
sutirtha sen <sutirthasen2u@gmail.com>
2023-11-12 11:32:10 +0530

    setup vercelfinal

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 const __dir = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
+//node version 16.17.1
 const tempelatePath = path.join(__dir, '../templates')
 const publicPath = path.join(__dir, '../public')
 //console.log(publicPath);
